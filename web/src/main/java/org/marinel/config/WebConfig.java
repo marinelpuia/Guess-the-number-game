37d872ae04afec7803f4bacfa502c38b6357b276
Marinel Puia <megatron1975@yahoo.co.uk>
2022-01-04 10:49:00 +0200

    Creating a custom interceptor by implementing the HandlerInterceptor interface

--- web/src/main/java/org/marinel/config/WebConfig.java
+++ web/src/main/java/org/marinel/config/WebConfig.java
@@ -1,15 +1,36 @@
 package org.marinel.config;
 
+import org.marinel.interceptor.RequestInterceptor;
 import org.marinel.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // == bean method for LocalResolver ==
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    // == register and open the app with the home view ==
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+
+    // == register RequestInterceptor.java ==
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
+
+        registry.addInterceptor(new LocaleChangeInterceptor());
     }
 }
